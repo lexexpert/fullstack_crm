056ea6b707f4e4b342be3eab08f1b7f0d8d4280b
Ivan <ivanreshetar@gmail.com>
2018-11-21 17:45:14 -0600

    added login function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,43 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
+const keys = require('../config/keys')
 
 
-module.exports.login = function (req, res) {
-    res.status(200).json({
-        email: req.body.email,
-        password: req.body.password
-    })
+module.exports.login = async function (req, res) {
+
+    const candidate = await User.findOne({email: req.body.email})
+
+    if(candidate){
+        //check login, user exist
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+        if(passwordResult){
+            //passwords matched, token generation
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 60 * 60});
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        }else{
+            //passwords doesn't match
+            res.status(401).json({
+                message: 'Incorrect password. Try again.'
+            })
+        }
+
+    }else{
+        //user doesn't exist, error
+        res.status(404).json({
+            message: 'User with this Email doesn\'t found'
+        })
+    }
+
+    // res.status(200).json({
+    //     email: req.body.email,
+    //     password: req.body.password
+    // })
 }
 
 module.exports.register = async function (req, res) {
