5ae7e7ca112e633f743b09cc93e9e5586eac8029
Ivan <ivanreshetar@gmail.com>
2018-11-20 21:57:43 -0600

    MongoDB connected

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
 const categoryRoutes = require('./routes/category')
 const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
+const keys = require('./config/keys')
 const app = express()
+
+mongoose.connect(keys.mongoURI)
+    .then(() => {
+        console.log('MongoDB connected')
+    })
+    .catch(error => console.log(error))
 
 app.use(require('morgan')('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
