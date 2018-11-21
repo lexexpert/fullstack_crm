b09ffc077e42423c61e4534ac1331d55aad14608
Ivan <ivanreshetar@gmail.com>
2018-11-20 23:12:50 -0600

    User registration function added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+
+
 module.exports.login = function (req, res) {
     res.status(200).json({
         email: req.body.email,
@@ -5,8 +9,34 @@ module.exports.login = function (req, res) {
     })
 }
 
-module.exports.register = function (req, res) {
-    res.status(200).json({
-        login: 'from controller'
-    })
+module.exports.register = async function (req, res) {
+
+    const candidate = await User.findOne({email: req.body.email})
+
+    if(candidate){
+        //User exist, show error
+        res.status(409).json({
+            message: 'This E-mail already exist. Try another one.'
+        })
+
+    }else{
+
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+
+        //Create user
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
+
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch (e) {
+            //handle error
+        }
+
+        //user.save().then(() => console.log('User created'))
+    }
 }
