a253447c648a1f9eabd6f42fa8162931a6db9afb
Ivan <ivanreshetar@gmail.com>
2018-11-21 19:10:26 -0600

    updated Position controller

--- controllers/position.js
+++ controllers/position.js
@@ -1,15 +1,56 @@
-module.exports.getByCategoryId = (req, res) => {
+const Position = require('../models/Positions')
+const errorHandler = require('../utils/errorHandler')
 
+
+module.exports.getByCategoryId = async (req, res) => {
+    try{
+        const positions = await Position.find({
+            category: req.params.categoryId,
+            user: req.user.id
+        })
+        res.status(200).json(positions)
+    }catch (e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.create = (req, res) => {
 
+module.exports.create = async (req, res) => {
+    try{
+        const position = await new Position({
+            name: req.body.name,
+            cost: req.body.cost,
+            category: req.body.category,
+            user: req.user.id
+        }).save()
+        res.status(201).json(position)
+    }catch (e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.remove = (req, res) => {
 
+module.exports.remove = async (req, res) => {
+    try{
+        await Position.remove(
+            {_id: req.params.id},
+            {$set: req.body},
+            {new: true}
+        )
+        res.status(200).json({
+            message: 'Position was removed'
+        })
+    }catch (e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.update = (req, res) => {
 
+module.exports.update = async (req, res) => {
+    try{
+        const position = await Position.findOneAndUpdate({_id: req.params.id})
+        res.status(200).json(position)
+    }catch (e) {
+        errorHandler(res, e)
+    }
 }
