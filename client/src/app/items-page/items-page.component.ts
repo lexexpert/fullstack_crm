4889e87b365cf976982bd686c980d337a4bf7ddc
Ivan <ivanreshetar@gmail.com>
2018-11-25 20:37:54 -0600

    pages added

---
+++ client/src/app/items-page/items-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-items-page',
+  templateUrl: './items-page.component.html',
+  styleUrls: ['./items-page.component.css']
+})
+export class ItemsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
